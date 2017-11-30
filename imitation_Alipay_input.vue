ee6fef51b022e5089a2adc94a8b331862abe9e95
Evan <wanlixi@users.noreply.github.com>
2017-11-30 14:59:43 +0800

    Update imitation_Alipay_input.vue

--- imitation_Alipay_input.vue
+++ imitation_Alipay_input.vue
@@ -1,47 +1,47 @@
 <template>
-	<div class="alipay">
-		<ul class="pwd" @click="blurInput">
-			<li
-				v-for="(item, index) in pwds" 
-				:key="index" 
-				:class="['pw', item.isInput ? 'is_inputed' : '']"></li>
-		</ul>
-		<input class="pwd_input" type="number" v-model="realPwd" maxlength="6">
-	</div>
+  <div class="alipay">
+     <ul class="pwd" @click="blurInput">
+	<li
+	  v-for="(item, index) in pwds" 
+	  :key="index" 
+	  :class="['pw', item.isInput ? 'is_inputed' : '']"></li>
+     </ul>
+     <input class="pwd_input" type="number" v-model="realPwd" maxlength="6">
+  </div>
 </template>
 <script>
 export default {
-	data () {
-		return {
-			pwds: [
-				{isInput: false},
-				{isInput: false},
-				{isInput: false},
-				{isInput: false},
-				{isInput: false},
-				{isInput: false},
-			],
-			realPwd: ''
-		}
-	},
-	watch: {
-		realPwd (realPwd) {
-			// realPwd = realPwd.substr(0,6);
-			// console.log(realPwd.length)
-			this.realPwd.length == 6 && (document.getElementsByClassName('pwd_input')[0].blur())
-			for (let i = 0; i < 6; i++) {
-				this.pwds[i].isInput = false;
-			}
-			for (let i = 0, j = realPwd.length; i < j; i++) {
-				this.pwds[i].isInput = true;
-			}
-		}
-	},
-	methods: {
-		blurInput () {
-			document.getElementsByClassName('pwd_input')[0].focus()
-		}
-	}
+  data () {
+    return {
+      pwds: [
+        {isInput: false},
+        {isInput: false},
+        {isInput: false},
+        {isInput: false},
+        {isInput: false},
+        {isInput: false},
+      ],
+      realPwd: ''
+    }
+  },
+  watch: {
+    realPwd (realPwd) {
+      // realPwd = realPwd.substr(0,6);
+      // console.log(realPwd.length)
+      this.realPwd.length == 6 && (document.getElementsByClassName('pwd_input')[0].blur())
+      for (let i = 0; i < 6; i++) {
+      	this.pwds[i].isInput = false;
+      }
+      for (let i = 0, j = realPwd.length; i < j; i++) {
+      	this.pwds[i].isInput = true;
+      }
+    }
+  },
+  methods: {
+    blurInput () {
+      document.getElementsByClassName('pwd_input')[0].focus()
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -71,14 +71,14 @@ $dottedWidth = .2rem;/* 10px */
   font-size .4rem/* 20px */
   position relative
 .is_inputed:before
-	content ''
-	position absolute
-	top (($inputLength - $dottedWidth) / 2)
-	left (($inputLength - $dottedWidth) / 2)
-	width $dottedWidth
-	height $dottedWidth
-	border-radius ($dottedWidth / 2)
-	background #000
+  content ''
+  position absolute
+  top (($inputLength - $dottedWidth) / 2)
+  left (($inputLength - $dottedWidth) / 2)
+  width $dottedWidth
+  height $dottedWidth
+  border-radius ($dottedWidth / 2)
+  background #000
 .pw:nth-last-child(1)
   border-right-width 1px
 .pwd_input
